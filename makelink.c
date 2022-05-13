a610b8511c31d86275491812debca48b44e87252
Benjamin Schichtholz <benjamin.schichtholz@mni.thm.de>
2022-05-13 17:35:39 +0200

    Hard-Link files from directory to other directory

--- makelink.c
+++ makelink.c
@@ -1,26 +1,58 @@
+#include "makelink.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-char* concat(const char *s1, const char *s2);
+#include <dirent.h>
 
 int main(int argc, char *argv[]){
   FILE *filesrc;
   FILE *filedest;
 
-  char *srcdir = "/tmp/test/";
-  char *destdir = "/tmp/test/";
+  char *srcdir = "./test/";
+  char *destdir = "./testlinks/";
 
-  if ((filesrc = fopen(concat(srcdir,"test.txt"), "r")) == NULL) {
-    printf("ERROR:\tfile not found\n");
+  DIR *d = opendir(srcdir);
+  struct dirent *dir;
+
+  // Open directory srcdir
+  if (d == NULL){
+    printf("ERROR:\tdirectory %s not found\n", srcdir);
+    exit(EXIT_FAILURE);
   } else {
-    printf("SUCC:\tfile exists!\n");
+    printf("Opened directory:%s\n", srcdir);
   }
 
-  link(concat(srcdir, "test.txt"),concat(destdir,"testhl.txt"));
+  // Read directory srcdir
+  while ((dir = readdir(d)) != NULL){
+    if (dir->d_type == DT_REG){
+      char* srcfile = concat(srcdir,dir->d_name);
+      char* destfile = concat(destdir,dir->d_name);
+      linkFile(srcfile,destfile);
+    }
+  }
 }
 
+// Create hard link destfile from srcfile
+void linkFile(const char *srcfile, const char *destfile) {
+  if (checkFile(srcfile)){
+    printf("... linking file %s\n",srcfile);
+    link(srcfile,destfile);
+  }
+  return;
+}
+
+// Check if file exists
+int checkFile(const char *filename) {
+  if (access(filename, F_OK) == 0) {
+    return 1;
+  } else {
+    printf("ERROR:\tfile %s not found\n",filename);
+    return 0;
+  }
+}
+
+// Concatenate two strings
 char* concat(const char *s1, const char *s2) {
     char *result = malloc(strlen(s1) + strlen(s2) + 1);
     if (result == NULL){
---
+++ makelink.h
@@ -0,0 +1,3 @@
+char* concat(const char *s1, const char *s2);
+int checkFile(const char *filename);
+void linkFile(const char *srcfile, const char *destfile);
